ba7e1a8d692bf81a625e4bff9a777721b8ecde2e
Aarsh Patel <142952200+aarshrpatel@users.noreply.github.com>
2025-02-16 06:32:30 +0000

    Complete Footer

--- short_trip/src/components/Footer/Footer.tsx
+++ short_trip/src/components/Footer/Footer.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Image from 'next/image'
+import logo from '@/assets/logo.png'
+import facebook from '@/assets/facebook-icon.png'
+import instagram from '@/assets/instagram-icon.png'
+import linkedin from '@/assets/linkedin-icon.png'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer>
-        <p className="text-center text-gray-500 text-xs">
-            &copy; {new Date().getFullYear()} Short Trip. All rights reserved.
-        </p>
+    <footer className='flex flex-col items-center justify-center space-y-4'>
+      {/* Line that separates footer from rest of page */}
+      <hr className='w-full'/>
+
+      {/* Short Trip Icon Logo */}
+      <Image src={logo} alt={"logo"} width={45} height={45}/>
+
+      {/* Links to other pages */}
+      <div className="flex space-x-4 text-xs"> 
+        <Link href="/" className="hover:text-red hover:underline transition-colors">Home</Link>
+        <Link href="/about" className="hover:text-red hover:underline transition-colors">Our Story</Link>
+        <Link href="/locations" className="hover:text-red hover:underline transition-colors">Locations</Link>
+        <Link href="/contact" className="hover:text-red hover:underline transition-colors">Contact Us</Link>
+      </div>
+
+      {/* Social Media Icons & Links */}
+      <div className="flex space-x-4 text-xs">
+        <Link href="https://www.facebook.com/shorttripstores/" target='_blank'><Image src={facebook} alt={"facebook logo"} className='w-8'/></Link>
+        <Link href="https://www.instagram.com/shorttripstore/" target='_blank'><Image src={instagram} alt={"instagram logo"} className='w-8'/></Link>
+        <Link href="https://www.linkedin.com/company/short-trip-stores/" target='_blank'><Image src={linkedin} alt={"linkedin logo"} className='w-8'/></Link>
+      </div>
+
+      {/* Copyright */}
+      <p className="text-center text-gray-500 text-xs">
+          &copy; {new Date().getFullYear()} Short Trip. All rights reserved.
+      </p>
     </footer>
   )
 }
--- short_trip/src/components/Navbar/Navbar.tsx
+++ short_trip/src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div>
             <div>
-                <Image src={logo} alt={"logo"} className={'flex fixed w-16 h-16 mx-6 my-5'}/>
+                <Image src={logo} alt={"logo"} className={'flex fixed mx-6 my-5'} width={60} height={60}/>
             </div>
             <nav
                 className="fixed top-0 left-0 w-full flex items-center justify-between backdrop-blur-lg bg-white/10 z-50">
@@ -15,8 +15,8 @@ const Navbar = () => {
                     className="font-bold fixed top-8 left-1/2 transform -translate-x-1/2 bg-mutecolor text-foreground px-2 py-2 rounded-full shadow-lg flex gap-6 text-lg">
                     <Link href="/" className="rounded-full px-4 hover:bg-red hover:text-mutecolor transition">Home</Link>
                     <Link href="/about" className="rounded-full px-4 hover:bg-red hover:text-mutecolor transition">Our Story</Link>
-                    <Link href="/" className="rounded-full px-4 hover:bg-red hover:text-mutecolor transition">Locations</Link>
-                    <Link href="/" className="rounded-full px-4 hover:bg-red hover:text-mutecolor transition">Contact Us</Link>
+                    <Link href="/locations" className="rounded-full px-4 hover:bg-red hover:text-mutecolor transition">Locations</Link>
+                    <Link href="/contact" className="rounded-full px-4 hover:bg-red hover:text-mutecolor transition">Contact Us</Link>
                 </div>
             </nav>
         </div>
