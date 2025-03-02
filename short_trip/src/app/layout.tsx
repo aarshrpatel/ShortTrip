807f6f357295177d53cd1011a5c425fb3844b176
pakshpatel1@gmail.com <pakshpatel1@gmail.com>
2025-03-02 14:48:52 -0500

    modify lines on landing

--- short_trip/src/app/layout.tsx
+++ short_trip/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import PageWrapper from "@/components/PageWrapper";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Enhanced SEO metadata
 export const metadata: Metadata = {
@@ -41,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-roboto">
-        <SpeedInsights />
+        {/*<SpeedInsights />*/}
         <Navbar />
         <PageWrapper>{children}</PageWrapper>
         <Footer />
